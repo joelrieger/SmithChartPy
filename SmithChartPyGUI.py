6d0a916200c65e042866a43546404afeab7c5376
Joel Rieger <rieger@gmail.com>
2016-10-23 21:45:17 -0700

    Update SmithChartPyGUI.py

--- SmithChartPyGUI.py
+++ SmithChartPyGUI.py
@@ -1,19 +1,27 @@
-from Tkinter import *
-root = Tk()
-im_size=150
+from plot_funcs import *
+from network_classes import *
 
+import matplotlib
+matplotlib.use('TkAgg')
+
+from numpy import arange, sin, pi
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+# implement the default mpl key bindings
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
+
+import sys
+if sys.version_info[0] < 3:
+    import Tkinter as Tk
+else:
+    import tkinter as Tk
+
+######################################################
+#SCHEM SETUP
+im_size=150
 box=None
 drag=False
 image_selection=0
-
-def down_click(event):
-    global image_selection
-
-    image_selection=int(event.x/im_size)
-    
-    canvas1.delete(box)
-    draw_box(canvas1,event.x/im_size)    
-
 
 def draw_box(canvas,n):
     global box
@@ -22,6 +30,13 @@ def draw_box(canvas,n):
                                   width=w,dash='.')
     canvas1.tag_raise(box)
 
+def down_click(event):
+    global image_selection
+
+    image_selection=int(event.x/im_size)
+    
+    canvas1.delete(box)
+    draw_box(canvas1,event.x/im_size)   
 
 def im_reorder(event,old,new):
     global image_selection
@@ -58,23 +73,71 @@ def reposition_image(event):
         drag=False
 
 
+######################################################
+root = Tk.Tk()
+root.wm_title("Embedding in TK")
+
+fig = Figure(figsize=(6, 6), dpi=100)
+fig.subplots_adjust(left=0.01, bottom=0.01, right=0.99, top=0.99)
+mainplot = fig.add_subplot(111,aspect=1)
+#t = arange(0.0, 3.0, 0.01)
+#s = sin(2*pi*t)
+
+PlotSmith(mainplot)
+#mainplot.plot(t, s)
+
+def _quit():
+    root.quit()     # stops mainloop
+    root.destroy()  # this is necessary on Windows to prevent
+                    # Fatal Python Error: PyEval_RestoreThread: NULL tstate
+
+
+    
+#PALETTE FRAME
+Palette = Tk.Frame(root)
+Palette.grid(row=1,column=0,sticky=Tk.N)
+button1=Tk.Button(Palette,text="SeriesC")
+button1.grid(row=0,column=0)
+button1=Tk.Button(Palette,text="ShuntC")
+button1.grid(row=0,column=1)
+button1=Tk.Button(Palette,text="SeriesL")
+button1.grid(row=1,column=0)
+button1=Tk.Button(Palette,text="SeriesL")
+button1.grid(row=1,column=1)
+entry1=Tk.Entry(Palette,width=5)
+entry1.grid(row=2,column=0)
+
+#PLOT FRAME
+PlotFrame = Tk.Frame(root)
+PlotFrame.grid(row=1,column=1)
+pltcanvas = FigureCanvasTkAgg(fig, master=PlotFrame)
+pltcanvas.get_tk_widget().grid(row=1,column=1)
+#NavigationToolbar2TkAgg uses pack internally, so grid cannot be used
+#Frame used to get around this.
+#NavBarFrame=Tk.Frame(root)
+#NavBarFrame.grid(row=1,column=1,sticky=Tk.W)
+#plttoolbar = NavigationToolbar2TkAgg(pltcanvas, NavBarFrame)
+#plttoolbar.pack()
+
+############################################################
+#SCHEMATIC FRAME
+SchemFrame = Tk.Frame(root)
+SchemFrame.grid(row=3,column=0,columnspan=2,sticky=Tk.W)
+
 capse = "icons/C.gif"
 capsh = "icons/Csh.gif"
-CapIconSe = PhotoImage(file=capse)
-CapIconSh = PhotoImage(file=capsh)
+CapIconSe = Tk.PhotoImage(file=capse)
+CapIconSh = Tk.PhotoImage(file=capsh)
 
 indse = "icons/Lse.gif"
 indsh = "icons/Lsh.gif"
-IndIconSe = PhotoImage(file=indse)
-IndIconSh = PhotoImage(file=indsh)
-
+IndIconSe = Tk.PhotoImage(file=indse)
+IndIconSh = Tk.PhotoImage(file=indsh)
 
 Match=[{'type':CapIconSh},{'type':IndIconSe},{'type':CapIconSe},{'type':IndIconSh}]
 
-canvas1 = Canvas(width=1000, height=150,scrollregion=(0,0,1000,1000))
-canvas1.pack(expand=1, fill=BOTH)
-canvas2 = Canvas(width=1000, height=150,scrollregion=(0,0,1000,1000))
-canvas2.pack(expand=1, fill=BOTH)
+canvas1 = Tk.Canvas(SchemFrame,width=750, height=150)#,scrollregion=(0,0,1000,1000)
+canvas1.grid(row=0,column=0)
 
 item_arr=[]
 x = (im_size)/2.0
@@ -89,4 +152,41 @@ canvas1.bind('<Button-1>', down_click)
 canvas1.bind('<B1-Motion>', follow_mouse)
 canvas1.bind('<ButtonRelease-1>', reposition_image)
 
-root.mainloop()
+#########################
+############################################################
+net=network()
+
+#TEST CASE -- matching 50.0 Ohms to ~5.0 Ohms at 2 GHz
+    
+L1=ind(0.80e-9)
+C1=cap(6.3e-12,shunt=1)    
+L2=ind(2.0e-9)
+C2=cap(1.6e-12,shunt=1)
+
+net.element_array.append(C2)
+net.element_array.append(L2)
+net.element_array.append(C1)
+net.element_array.append(L1)
+
+#Draw
+freq=2.0e9
+curves=[]
+nodeZ=net.compute_node_impedances(freq)
+for i in range(len(nodeZ)-1):
+    if net.element_array[i].orientation==0:
+        curves.append(ConstImpedCurve(nodeZ[i],nodeZ[i+1],100))
+        mainplot.plot(curves[i][0],curves[i][1],lw=3,color='b')
+    elif net.element_array[i].orientation==1:
+        curves.append(ConstAdmitCurve(nodeZ[i],nodeZ[i+1],100))
+        mainplot.plot(curves[i][0],curves[i][1],lw=3,color='b')
+        
+for i in range(len(nodeZ)):
+    Gam=ZtoGamma(nodeZ[i])
+    print Gam
+    mainplot.plot(Gam.real,Gam.imag,'or')
+
+##########################
+    
+Tk.mainloop()
+# If you put root.destroy() here, it will cause an error if
+# the window is closed with the window manager.
